66b8ee470782af176e1a646a018f566865501129
Zain Rajani <zainrajani2207@gmail.com>
2020-11-16 08:58:18 -0500

    Code Updation
    Update the code with the integration of the LDR Functionality along with the other functionality being the same as previous commits

--- Codes/Project_Programming/iLetterbox.c
+++ Codes/Project_Programming/iLetterbox.c
@@ -6,8 +6,8 @@
    
 /* Libraries to be included here */
 
-#include <stdlib.h>
-#include <stdio.h>
+#include <stdlib.h>        // For system(...) command to run some external commands in C Program 
+#include <stdio.h>        // For Standard Input Output Function like printf(), scanf() and more
 #include <unistd.h>      // For usleep function
 #include <iobb.h>       // To access the GPIO pins easily library already downloaded and installed
 
@@ -22,31 +22,35 @@ int main()
   //Set the pin function either as input or output pins
   iolib_setdir(9,15,DigitalIn);
   iolib_setdir(8,11,DigitalOut);
-  
+  iolib_setdir(9,41,DigitalIn);
+
   //Setting initially output pin low 
   pin_low(8,11);         //P8.11 Low (0)
   
   // Loops forever 
   while(1)
   {
-    // Check the input value of the pin 
-    if (is_low(9,15))
+     // Check the input value of the pin
+    // Condition: If IR Sensor sensed arrival of the letter and brightness is there then 
+    if ((is_low(9,15)) && (is_low(9,41)))
     {
       pin_high(8,11);
       system("/usr/bin/python2.7 pysms.py");
       counter++;
-      printf("Letter # %d\n",counter);
-      usleep(80000);                  // Suspend the process for the given duration
-    }
-    if (is_high(9,15))
+      printf("Letter # %d \n",counter);
+      usleep(100000);
+   }
+    // If no letter is sensed and there is darkness then
+    if ((is_high(9,15)) && (is_high(9,41)))
     {
-      // If sensor detects no object do nothing
+      // If sensor detects no object do nothing only reset the counter if there is darkness also
+      counter=0;
+      pin_low(8,11);
     }
+
   }
-  
 // Free up the resources like the GPIO pins for other use if required from memory
 iolib_free();
 
 return (0);     // If code success then return 0 else -1
-
 }
