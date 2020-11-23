694002491e2f18982a570a8e9e43d439c7d7d17c
Zain Rajani <zainrajani2207@gmail.com>
2020-11-23 00:24:35 -0500

    Added comments to final code

--- Codes/Project_Programming/Final Code.cpp
+++ Codes/Project_Programming/Final Code.cpp
@@ -6,28 +6,30 @@
    
 /* Libraries to be included here */
 
-#include <stdlib.h>        // For system(...) command to run some external commands in C Program 
-#include <stdio.h>        // For Standard Input Output Function like printf(), scanf() and more
-#include <unistd.h>      // For usleep function
-#include <iobb.h>       // To access the GPIO pins easily library already downloaded and installed
-#include <iostream>
-#include <sstream>
-#include <fstream>
-#include "exploringBB/chp11/thingSpeak/network/SocketClient.h"
+#include <stdlib.h>          // For system(...) command to run some external commands in C Program 
+#include <stdio.h>          // For Standard Input Output Function like printf(), scanf() and more
+#include <unistd.h>        // For usleep function
+#include <iobb.h>         // To access the GPIO pins easily library already downloaded and installed
+#include <iostream>      //  For input output cin, cout functions
+#include <sstream>      //  string stream classes like ostringstream (output string)
+#include <fstream>     //  Read and write file system **Can be ignored as we dont deal with any files
+#include "exploringBB/chp11/thingSpeak/network/SocketClient.h"   // Internet Protocol Family Header 
 
-int thingspeak (int data)
+int thingspeak (int count)
 {
    ostringstream head,data;
-   SocketClient sc ("api.thingspeak.com",80);
-   data << "field1="<<data<<endl;
-   sc.connectToServer();
+   SocketClient sc ("api.thingspeak.com",80);    // object for API to the ThingSpeak Website using HTTP port 80
+   data << "field1="<<count<<endl;               // Data to be sent to the cloud and which feild to store it 
+   sc.connectToServer();                        // Connect to the server (request)
+   // Use the HTTP methods to get the request sent to the cloud 
    head<<"POST /update HTTP/1.1\n"<<"Host: api.thingspeak.com\n"
    <<"Connection: close\n"<<"X-THINGSPEAKAPIKEY:VMCNRV25Y3J1XTOZ\n"
    <<"Content-type: application/x-www-form-urlencoded\n"<<"Content-Length:"
    <<string(data.str()).length()<<"\n\n";
+   // Send Data 
    sc.send (string(head.str()));
    sc.send (string(data.str()));
-   sleep(10);
+   sleep(15);
 }
 
 
@@ -58,7 +60,7 @@ int main()
       system("/usr/bin/python2.7 pysms.py");
       counter++;
       printf("Letter # %d \n",counter);
-      thingspeak(counter);
+      thingspeak(counter);     // Call the cloud function created 
       usleep(100000);
    }
     // If no letter is sensed and there is darkness then
@@ -69,7 +71,7 @@ int main()
       printf("Counter Resetted # %d\n",counter);
       pin_low(8,11);
       system("/usr/bin/python2.7 pysend.py");
-      thingspeak(counter);
+      thingspeak(counter);     // Call the function to connect and send data to the cloud 
     }
 
   }
